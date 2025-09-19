6d1cd720f4950118a799ec70bfcacdebe00193c4
Uygar <121864027+uygar-gormez28@users.noreply.github.com>
2025-09-19 19:43:45 +0300

    cliclk events
    buttonlar ile event tetikleme kısımlarını yaptım

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //todo sayfa tamamen yüklendiğind eçalışacak kodlar
+  const generateBtn = document.querySelector(".generate-btn");
+  const saveBtn = document.querySelector(".save-btn");
+  const qrCodeContainer = document.querySelector(".qr-code");
+  let qrCodeInstance = null;
+
+  generateBtn.addEventListener("click", (e) => {
+    //btn click olduğunda qr oluşturur
+    //kullanıcığın girdiği text/url
+    let qrText = document.querySelector(".qr-text").value;
+
+    if (qrCodeInstance) {
+      qrCodeInstance.clear(); //önceki qr sil
+      qrCodeInstance = null; //qr kod boş ata
+      qrCodeContainer.innerHTML = ""; //qr kod container temizle
+    }
+    if (qrText) {
+      //kullanıcının girdiği text boş değilse qr oluşturacak
+      //! Qr oluşturma kısmına tekrar bakılmalı
+      qrCodeInstance = new QRCode(qrCodeContainer, { text: qrText, with: 128, height: 128 });
+
+      //qr kod animayonlu görünmesi
+      qrCodeContainer.style.opacity = "1";
+      qrCodeContainer.style.transform = "scale(1)";
+    }
+  });
+});
